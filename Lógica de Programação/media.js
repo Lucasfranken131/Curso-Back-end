b02c3954869bd279cefaf13a84e793313d0d40c2
Lucas Franco <105955764+Lucasfranken131@users.noreply.github.com>
2023-03-07 17:14:47 -0300

    Delete media.js

--- Lógica de Programação/media.js
+++
@@ -1,4 +0,0 @@
-//um grupo de professores está tendo dificuldade em registrar as notas dos alunos e realizar os cálculos de aprovação. 
-//Além disso precisam informar pessoalmente, a cada aluno essas aprovações, resultando em muita desorganização.
-//Sendo assim, precisam encontrar uma forma de realizar essa gestão com eficiência
-//*condição para aprovação de um aluno a média de todas a notas deve ser 7

a4e2b4def9a69bc8e0ee62a92444b9dc42847908
Lucas Franco <105955764+Lucasfranken131@users.noreply.github.com>
2023-07-12 17:20:01 -0300

    Add files via upload

---
+++ Pedra_Papel_Tesoura/index.js
@@ -0,0 +1,21 @@
+
+
+function mudarComputador(){
+    computer_image = document.getElementById['Computador']
+    return computer_image;
+}
+
+function Pedra(){
+    var player = document.getElementById('image'); 
+    player.src="rock.png";
+}
+
+function Papel(){
+    var player = document.getElementById('image'); 
+    player.src="paper.png";
+}
+
+function Tesoura(){
+    var player = document.getElementById('image'); 
+    player.src="scissors.png";
+}
